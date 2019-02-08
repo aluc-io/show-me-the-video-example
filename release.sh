200bd571e7cbd0c9030d0df774fc64d41f6bed7a
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2019-02-08 20:20:13 +0900

    fix: docker hub automated build error

--- release.sh
+++ release.sh
@@ -1,3 +1,11 @@
-sed -e "s/%VERSION%/$(git describe)/g" index.html.template > index.html
-heroku container:push web
+CONTEXT_DIR=context-heroku
+rm -rfv $CONTEXT_DIR
+mkdir $CONTEXT_DIR
+cp Dockerfile-heroku $CONTEXT_DIR/Dockerfile
+cp download-static-resource.sh $CONTEXT_DIR/
+cp index.html.template $CONTEXT_DIR/
+cp mysite.template $CONTEXT_DIR/
+
+cd $CONTEXT_DIR
+heroku container:push web --arg CACHE_TAG=$(git describe)
 heroku container:release web
